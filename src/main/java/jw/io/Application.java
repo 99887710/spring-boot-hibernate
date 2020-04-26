09db1e8fe00a08a146e2eb169b8a3532c6e0d4f4
jeff wu <j98728@hotmail.com>
2020-04-26 05:29:51 +0100

    Init project

---
+++ src/main/java/jw/io/Application.java
@@ -0,0 +1,19 @@
+package jw.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import jw.io.controller.PingController;
+
+
+@SpringBootApplication
+// We use direct @Import instead of @ComponentScan to speed up cold starts
+// @ComponentScan(basePackages = "jw.io.controller")
+@Import({ PingController.class })
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/jw/io/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package jw.io.repository;
+
+import jw.io.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
